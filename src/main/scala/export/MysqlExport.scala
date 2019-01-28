71161c12b8b74d09c6b5bde2575184bf5e122103
Ashfaq Salehin <ashfaq.aws@gmail.com>
2019-01-28 20:11:41 +0700

    Mysql driver not found issue fixed

--- src/main/scala/export/MysqlExport.scala
+++ src/main/scala/export/MysqlExport.scala
@@ -14,6 +14,7 @@ import org.apache.spark.sql.SaveMode
 
 object MysqlExport {
   val mysqlConf = conf.parseMysqlConf
+  Class.forName("com.mysql.cj.jdbc.Driver")
   
   def getJdbcUrl () : String = {
     s"jdbc:mysql://${this.mysqlConf("host")}:${this.mysqlConf("port")}/${this.mysqlConf("database")}"
